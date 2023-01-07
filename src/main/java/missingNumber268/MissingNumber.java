d606a140f09408b380ebd3d8f170f35b185205db
John Doe <johndoe@example.com>
2023-01-07 22:29:17 +0500

    Missing Number without comments

--- src/main/java/missingNumber268/MissingNumber.java
+++ src/main/java/missingNumber268/MissingNumber.java
@@ -10,33 +10,13 @@ public class MissingNumber {
     }
 
     public int missingNumber(int[] nums) {
-        //Arrays.stream(nums).sorted()
-
         Arrays.sort(nums);
-        int[] idealArr = IntStream.range(0, nums.length+1).toArray();
-
-//        System.out.println("nums = " + Arrays.toString(nums));
-//        System.out.println("idealArr = " + Arrays.toString(idealArr));
-
-//        int result=0;
-//        boolean isLast=true;
+        int[] idealArr = IntStream.range(0, nums.length + 1).toArray();
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i]!=idealArr[i]){
+            if (nums[i] != idealArr[i]) {
                 return i;
-                //result=i;
-                //isLast = false;
             }
         }
-
-//        if (result==0){
-//            result=nums.length;
-//        }
-//        if (Arrays.equals(nums, new int[]{1})){
-//            result=0;
-//        }
-
-        //result = isLast ? nums.length : result;
-        //return isLast ? nums.length : result;
         return nums.length;
     }
 }

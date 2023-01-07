6306b95ff8b78bf4fac20a572180cb194faf1ce8
John Doe <johndoe@example.com>
2023-01-08 00:58:35 +0500

    Single Number without comments

---
+++ src/main/java/singleNumber136/SingleNumber.java
@@ -0,0 +1,11 @@
+package singleNumber136;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int n : nums) {
+            result ^= n;
+        }
+        return result;
+    }
+}

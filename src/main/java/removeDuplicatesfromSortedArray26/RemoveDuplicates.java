43caa5aebc83a3462d9b8e8c6a478187587b5d82
John Doe <johndoe@example.com>
2023-01-21 18:06:29 +0500

    26 Remove Duplicates from Sorted Array without comments

--- src/main/java/removeDuplicatesfromSortedArray26/RemoveDuplicates.java
+++ src/main/java/removeDuplicatesfromSortedArray26/RemoveDuplicates.java
@@ -7,22 +7,12 @@ public class RemoveDuplicates {
     }
 
     public int removeDuplicates(int[] nums) {
-//        for (int i = 0; i < nums.length; i++) {
-//            if (nums[i]==nums[i+1]){
-//                nums[i+1]=nums[i+2];
-//            }
-//        }
-//        int p1;
-//        int p2;
-//        while (){
-//
-//        }
         int unic = 0;
-        for (int i = 1; i <nums.length; i++) {
-            if (nums[unic]!=nums[i]){
-                nums[++unic]=nums[i];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[unic] != nums[i]) {
+                nums[++unic] = nums[i];
             }
         }
-        return unic+1;
+        return unic + 1;
     }
 }

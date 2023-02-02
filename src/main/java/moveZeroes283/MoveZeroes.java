27262cb7a74050cbfa8b0dd6aeb2f5653db6da3f
John Doe <johndoe@example.com>
2023-02-03 00:36:52 +0500

    283 Move Zeroes without comments

--- src/main/java/moveZeroes283/MoveZeroes.java
+++ src/main/java/moveZeroes283/MoveZeroes.java
@@ -2,25 +2,15 @@ package moveZeroes283;
 
 public class MoveZeroes {
 
-    public void moveZeroes(int[] nums){
+    public void moveZeroes(int[] nums) {
         int zero = 0;
-        //int nonZero = 0;
-//        for (int n: nums) {
-//            if (n==0) {
-//                nonZero++;
-//            } else {
-//                zero++;
-//            }
-        for (int i = 0; i < nums.length && zero<nums.length; i++) {
-            if (nums[i]!=0 && nums[zero]==0){
-                nums[zero]=nums[i];
-                nums[i]=0;
+        for (int i = 0; i < nums.length && zero < nums.length; i++) {
+            if (nums[i] != 0 && nums[zero] == 0) {
+                nums[zero] = nums[i];
+                nums[i] = 0;
                 zero++;
             }
-
-            if (nums[zero]!=0) {
-//                nums[zero]=nums[i];
-//                nums[i]=0;
+            if (nums[zero] != 0) {
                 zero++;
             }
         }
